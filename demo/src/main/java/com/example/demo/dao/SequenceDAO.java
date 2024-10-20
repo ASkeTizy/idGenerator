5f0156c93abbe0def68291a8ee1b60f16d46e9b3
Artiom <artemnevmerz@gmail.com>
2024-10-20 23:28:45 +0300

    JdcClienAdded

--- demo/src/main/java/com/example/demo/dao/SequenceDAO.java
+++ demo/src/main/java/com/example/demo/dao/SequenceDAO.java
@@ -2,71 +2,65 @@ package com.example.demo.dao;
 
 import com.example.demo.entity.Book;
 import com.example.demo.entity.EntityIdentifier;
+import com.example.demo.entity.SequenceData;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Repository;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+@Repository
 public class SequenceDAO {
     private static final String jdbcUrl = "jdbc:postgresql://localhost:54320/postgres";
     private static final String username = "postgres";
     private static final String password = "12345";
-    private final Connection connection;
-    private final Map<String,Long> limitValues = new HashMap<String,Long>();
-    private final Map<String,Long> currentValues = new HashMap<String,Long>();
-    public SequenceDAO()  {
-        try {
-            Class.forName("org.postgresql.Driver");
-            this.connection = DriverManager.getConnection(jdbcUrl, username, password);
-            getData();
-        } catch (SQLException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final JdbcClient jdbcClient;
+    private final Map<String, Long> limitValues = new HashMap<>();
+    private final Map<String, Long> currentValues = new HashMap<String, Long>();
 
-
+    public SequenceDAO() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        this.namedParameterJdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+        this.jdbcClient = JdbcClient.create(dataSource);
+        getData();
     }
+
     public Long generateId(String type) {
         var limit = limitValues.get(type);
         var currentValue = currentValues.get(type);
         currentValue++;
-        currentValues.put(type,currentValue);
-        if( limit <= currentValue ) {
-
-            PreparedStatement statement = null;
-            try {
-                statement = this.connection.prepareStatement("UPDATE \"LIBRARY\".sequencedata SET ID=? WHERE role=?");
-                statement.setLong(1, limit + 100L);
-                statement.setString(2, type);
-                limitValues.put(type,limit+100L);
-                statement.execute();
-                connection.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+        currentValues.put(type, currentValue);
+        if (limit <= currentValue) {
+            this.jdbcClient.sql("UPDATE \"LIBRARY\".sequencedata SET ID=:id WHERE role=:role")
+                    .param("id",limit + 100L)
+                    .param("role",type)
+                    .update();
+            limitValues.put(type, limit + 100L);
         }
-        ;
         return currentValues.get(type);
     }
+
     public void getData() {
-        try {
-            var statement = this.connection.prepareStatement("SELECT * FROM \"LIBRARY\".sequencedata");
-
-            ResultSet resultSet = statement.executeQuery();
-            List<Book> books = new ArrayList<>();
-
-            while (resultSet.next())
-            {
-                String role = resultSet.getString("role");
-                Long id = resultSet.getLong("id");
-                limitValues.put(role,id);
-                currentValues.put(role,id);
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        limitValues.putAll(this.jdbcClient.sql("SELECT * FROM \"LIBRARY\".sequencedata")
+                .query().listOfRows().stream().collect(Collectors.toMap(s -> (String) s.get("role"),
+                        s -> (Long) s.get("id"))));
+        currentValues.putAll(limitValues);
     }
-
 }
